f0f94aac8f96555fbe6a96481c4770ad820ff317
William Aguilar <44141394+thewill-i-am@users.noreply.github.com>
2024-11-01 06:10:30 -0600

    Clean up ruff

--- tests/test_genetic_optimizer.py
+++ tests/test_genetic_optimizer.py
@@ -1,12 +1,14 @@
 import unittest
+
 from qutip import Options
+
 from circuits.deutsch_jozsa_circuit import DeutschJozsaCircuit
 from src.evaluator import Evaluator
 from src.genetic_optimizer import GeneticOptimizer
 from src.noise_model import NoiseModel
 
-# Function moved outside for test_early_stopping
-def constant_evaluate(individual):
+
+def constant_evaluate():
     return (0.8,)
 
 def counting_evaluate(individual):
@@ -43,7 +45,7 @@ class TestGeneticOptimizer(unittest.TestCase):
         population, logbook = self.optimizer.run(csv_filename="test_genetic_algorithm_log.csv")
         self.assertTrue(len(population) > 0)
         self.assertIsInstance(population[0], dict)
-        self.assertTrue(hasattr(population[0], 'fitness'))
+        self.assertTrue(hasattr(population[0], "fitness"))
         self.assertTrue(population[0].fitness.valid)
         self.assertTrue(len(logbook) <= self.optimizer.num_generations)
 
@@ -54,7 +56,7 @@ class TestGeneticOptimizer(unittest.TestCase):
         original_individual1 = individual1.copy()
         original_individual2 = individual2.copy()
         self.optimizer.crossover_probability = 1.0
-        offspring1, offspring2 = self.optimizer._cx_dict(individual1, individual2)
+        offspring1, offspring2 = self.optimizer._cx_dict(individual1, individual2)  # noqa: SLF001
         self.assertNotEqual(offspring1, original_individual1)
         self.assertNotEqual(offspring2, original_individual2)
 
@@ -62,7 +64,7 @@ class TestGeneticOptimizer(unittest.TestCase):
         self.optimizer.mutation_probability = 0.2
         self.optimizer.crossover_probability = 0.5
         self.optimizer.last_avg_fitness = 0.9
-        self.optimizer._adjust_probabilities(current_avg_fitness=0.905)
+        self.optimizer._adjust_probabilities(current_avg_fitness=0.905)  # noqa: SLF001
         self.assertAlmostEqual(self.optimizer.mutation_probability, 0.25)
         self.assertAlmostEqual(self.optimizer.crossover_probability, 0.55)
 
@@ -70,7 +72,7 @@ class TestGeneticOptimizer(unittest.TestCase):
         self.optimizer.mutation_probability = 0.2
         self.optimizer.crossover_probability = 0.5
         self.optimizer.last_avg_fitness = 0.9
-        self.optimizer._adjust_probabilities(current_avg_fitness=0.915)
+        self.optimizer._adjust_probabilities(current_avg_fitness=0.915) # noqa: SLF001
         self.assertAlmostEqual(self.optimizer.mutation_probability, 0.15)
         self.assertAlmostEqual(self.optimizer.crossover_probability, 0.45)
 
@@ -83,7 +85,6 @@ class TestGeneticOptimizer(unittest.TestCase):
         self.evaluator.evaluate = original_evaluate
 
     def test_population_diversity(self):
-        num_qubits = 3
         self.solver_options = Options(nsteps=100000, store_states=True)
 
         evaluator = Evaluator(self.circuit, self.noise_model, self.solver_options)
@@ -103,18 +104,18 @@ class TestGeneticOptimizer(unittest.TestCase):
         self.assertTrue(len(unique_individuals) > 1)
 
     def test_toolbox_setup(self):
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'individual'))
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'population'))
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'mate'))
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'mutate'))
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'select'))
-        self.assertTrue(hasattr(self.optimizer.toolbox, 'evaluate'))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "individual"))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "population"))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "mate"))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "mutate"))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "select"))
+        self.assertTrue(hasattr(self.optimizer.toolbox, "evaluate"))
 
     def test_invalid_evaluator(self):
         with self.assertRaises(AttributeError):
             invalid_evaluator = object()
             optimizer = GeneticOptimizer(evaluator=invalid_evaluator)
-            optimizer._setup_toolbox()
+            optimizer._setup_toolbox() # noqa: SLF001
 
 if __name__ == "__main__":
     unittest.main()
