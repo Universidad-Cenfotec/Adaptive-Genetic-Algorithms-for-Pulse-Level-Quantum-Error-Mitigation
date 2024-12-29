762ced386c4d5d1c2d22fe3f1b140eb9e488a02a
William Aguilar <44141394+thewill-i-am@users.noreply.github.com>
2024-12-28 18:23:07 -0600

    Refactor code move logic from main to visualizer

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from src.csv_logger import CSVLogger
 from src.evaluator import Evaluator
 from src.genetic_optimizer import GeneticOptimizer
 from src.noise_model import NoiseModel
@@ -6,10 +7,11 @@ from src.quantum_utils import QuantumUtils
 from src.visualizer import Visualizer
 
 __all__ = [
+    "CSVLogger",
     "Evaluator",
     "GeneticOptimizer",
     "NoiseModel",
     "QuantumCircuitBase",
     "QuantumUtils",
-    "Visualizer",
+    "Visualizer"
 ]
